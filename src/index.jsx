46ce8ab8d5810f09311a3a27a7555cedebe36c90
Aleksandrina Karakehayova <akarakehayova@axway.com>
2019-02-06 08:59:36 +0200

    Fixed game table

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, Switch} from 'react-router-dom'
-
+import "react-table/react-table.css";
+import history from './history'
 
 // components and pages
 import App from './App.jsx'
 
 ReactDOM.render((
-  <Router>
+  <Router history={history}>
     <Switch>
       <Route exact path='/' component={App} />
 
