923128e0b82d3c206c2156c94b9fc332978c74bb
Hukyy <hukyy@abv.bg>
2019-02-07 16:41:47 +0200

    Games spider crawls for geek, average rating and number of votes

--- ForumCrawler/ForumCrawler/spiders/games_crawler.py
+++ ForumCrawler/ForumCrawler/spiders/games_crawler.py
@@ -1,5 +1,6 @@
 import scrapy
 import io
+import re
 
 class GamesCrawler(scrapy.Spider):
     name = "games"
@@ -16,9 +17,34 @@ class GamesCrawler(scrapy.Spider):
 
     def parse(self, response):
         names = response.xpath('//tr[@id="row_"]//td[3]//a/text()').extract()
-        file_name = 'games.txt'
-        with io.open(file_name, "a", encoding="utf-8") as f:
-            f.writelines("\n".join(names))
+        geek_rating = response.xpath('//tr[@id="row_"]//td[4]/text()').extract()
+        avg_rating = response.xpath('//tr[@id="row_"]//td[5]/text()').extract()
+        num_votes = response.xpath('//tr[@id="row_"]//td[6]/text()').extract()
+
+        size = len(names)
+        crop_left = re.compile(r'[\n\t\t\t]')
+        crop_right = re.compile(r'[\t\t]')
+        for i in range(size):
+            name = names[i]
+            g_rating = geek_rating[i]
+            a_rating = avg_rating[i]
+            votes = num_votes[i]
+
+            name = re.sub(crop_left, '', name)
+            name = re.sub(crop_right, '', name)
+            g_rating = re.sub(crop_left, '', g_rating)
+            g_rating = re.sub(crop_right, '', g_rating)
+            a_rating = re.sub(crop_left, '', a_rating)
+            a_rating = re.sub(crop_right, '', a_rating)
+            votes = re.sub(crop_left, '', votes)
+            votes = re.sub(crop_right, '', votes)
+
+            yield {
+                "name" : name,
+                "geek_rating" : g_rating,
+                "avg_rating" : a_rating,
+                "num_votes" : votes
+            }
 
         next_page = response.css('div.fr').xpath('//a[@title="next page"]/@href').extract_first()
         if next_page:
