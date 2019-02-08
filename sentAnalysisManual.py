fc6f3417fbb71f2306289df05825eb3ac388ae49
AKarakehayova <alexzk95@gmail.com>
2019-02-08 07:52:03 +0200

    Added sentiment analysis based on naive bayes classifier

--- sentAnalysisManual.py
+++ sentAnalysisManual.py
@@ -1,10 +1,17 @@
 from textblob import TextBlob
+from textblob.classifiers import NaiveBayesClassifier
 import glob
 import json
 
 files = glob.glob('./SearchEngine/*.txt')
-classification = open("classification.json", "w")
+classification = open("classification2.json", "w")
 games = []
+
+
+with open('test.csv', 'r') as fp: 
+    cl = NaiveBayesClassifier(fp, format="csv")
+
+
 for file in files:
     with open(file) as f:
         lines = [line.rstrip('\n') for line in f]
@@ -13,18 +20,20 @@ for file in files:
         counter = 0
         
         for line in lines:
-            tb = TextBlob(line)
-            polarity = polarity + tb.sentiment.polarity
-            subjectivity = subjectivity + tb.sentiment.subjectivity
-            counter = counter + 1
+            tb = TextBlob(line, classifier=cl)
+            prob_dist = cl.prob_classify(tb)
+            posProb = round(prob_dist.prob("pos"), 2)
+            polarity = polarity + posProb
+            counter = counter + 1 
+
         gameName = file.replace("./SearchEngine/", "")
         gameName = gameName.replace(".txt", "") #todo regex 
         newLine = {
             'name': gameName,
             'count': counter,
-            'polarity': polarity/counter,
-            'subjectivity': subjectivity/counter
-        }
+            'polarity': polarity/counter
+        }    
+     
         games.append(newLine)
 
 classification.write(json.dumps(games))
